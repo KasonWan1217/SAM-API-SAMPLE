29a95b708df1998f28ad20c9790c4c884679134c
KasonWan <kasonwan1217@gmail.com>
2021-02-26 18:09:06 +0800

    initial

---
+++ SAM-API-SAMPLE/HelloWorldFunction/src/main/java/stream/dbStream.java
@@ -0,0 +1,12 @@
+package stream;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+
+import java.util.logging.StreamHandler;
+
+public class dbStream {
+    public StreamHandler handleRequest() {
+        return null;
+    }
+}
